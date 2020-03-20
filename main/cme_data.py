63313d78dc9e39137c273321797b1131d29414c8
dominik <dominwro@gmail.com>
2020-03-20 20:39:44 +0100

    Add cme_data

--- main/cme_data.py
+++ main/cme_data.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import simplefix as sf
+from simplefix import *
 
 #data_feed = pd.read_csv('/home/dominik/PycharmProjects/CMEData/6E_20060207.TXT', delimiter="\t")
-data_feed = pd.read_csv('/home/dominik/PycharmProjects/CMEData/6E_20060207.TXT', parse_dates=True)
-
+data_feed = pd.open('/home/dominik/PycharmProjects/CMEData/6E_20060207.TXT', 'r')
+data_feed = data_feed.decode('utf-8')
 print(data_feed)
 
 #cme_data = sf.FixParser.add_raw('6E_20060207.TXT')
