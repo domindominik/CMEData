dfb03926ed297195f63dd213fc5bce0ef5933fb2
dominik <dominwro@gmail.com>
2020-03-22 16:57:16 +0100

    Add compiler_data() method

--- main/get_sp500_data.py
+++ main/get_sp500_data.py
@@ -18,7 +18,7 @@ def get_sp500_tickers():
     with open("sp500tickers.pickle", "wb") as f:
         pickle.dump(tickers, f)
     return tickers
-print(get_sp500_tickers())
+#print(get_sp500_tickers())
 
 def get_data_from_yahoo(reload_sp500=False):
     if reload_sp500:
@@ -38,7 +38,30 @@ def get_data_from_yahoo(reload_sp500=False):
             df = web.DataReader(ticker.replace('.', '-'), 'yahoo', start, end)
             df.to_csv('stock_dfs/{}.csv'.format(ticker))
         else:
-            print('Already have {}')
+            print('Already have {}'.format(ticker))
 
-get_data_from_yahoo()
+#get_data_from_yahoo()
 
+def compile_data():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count, ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+        df.rename(columns={'Adj Close': ticker}, inplace=True)
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+
+    print(main_df.head())
+    main_df.to_csv('stock_dfs/sp500_joined_closes.csv')
+
+compile_data()
